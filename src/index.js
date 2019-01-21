75d083754db1c23558ae4a6ca0b498b15d25ee55
eamarelo <eamarelo@hotmail.fr>
2019-01-21 21:04:55 +0100

    Revert "create registration component"
    This reverts commit e738ce92fa11674c8fd0220de66c0e5440676eb0.

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
-// Dependencies
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-
-// Functions
-import store from './store'
-
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './style.scss'
-
-// Components
-import Routes from './routes'
 
 const App = () => (
-  <Provider store={store}>
-    <Routes />
-  </Provider>
+  <div>
+    <h1>Live Class</h1>
+  </div>
 )
 
 ReactDOM.render(<App />, document.getElementById('app'))
--- src/reducers.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux'
-
-import home from './components/home/homeReducer'
-
-export default combineReducers({
-  home
-})
